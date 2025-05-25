3a5d9bcb189edcb196a59083cbd4e551e9792c6f
antonydsp13 <antonydsp13@gmail.com>
2025-05-25 13:34:45 +0530

    Add files via upload
    Day 01 Assignment

---
+++ Day01/Assignment_PrimitiveDataTypes.js
@@ -0,0 +1,19 @@
+//
+let FirstName='Prince';
+let CompanyName='MSC Technology';
+let MobileNumber=9003053024;
+let IsAutomation=true;
+let Hasplaywright;
+
+console.log(" FirstName is "+FirstName);
+console.log("FirstName Data Type is "+ typeof FirstName);
+console.log("CompanyName is "+CompanyName);
+console.log("CompanyName Data Type is "+ typeof CompanyName);
+console.log("MobileNumber is "+MobileNumber);
+console.log("MobileNumber Data Type is "+ typeof MobileNumber);
+console.log("IsAutomation is "+IsAutomation);
+console.log("IsAutomation Data Type is "+ typeof IsAutomation);
+console.log("Hasplaywright is "+Hasplaywright);
+console.log("Hasplaywright Data Type is "+ typeof Hasplaywright);
+
+
